ba62f9cb9279b2733ed023e28f34e067cea30de7
stevenmars <37410353+stevenmars@users.noreply.github.com>
2018-03-15 15:48:51 +0000

    Add files via upload

---
+++ src/EmployeeDiary.java
@@ -0,0 +1,95 @@
+import java.util.Set;
+
+/**
+ * 
+ */
+
+/**
+ * @author steve
+ *
+ */
+public class EmployeeDiary {
+
+	private Set<Integer> Meetings;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	/*
+	 * method to add an entry to the Employee's diary
+	 * 
+	 */
+	public void addEntry()
+	{
+		
+	}
+	
+	/*
+	 * method to delete an entry from the Employee's diary
+	 * 
+	 */
+	public void deleteEntry() 
+	{
+		
+	}
+	
+	/*
+	 * method to edit an Employee's entry in their diary
+	 * 
+	 */
+	public void editEntry() 
+	{
+		
+	}
+	
+	/*
+	 * method to delete the Employee's last entry from their diary
+	 * 
+	 */
+	public void deleteLastEntry() 
+	{
+		
+	}
+	
+	/*
+	 * method to save the Employee's diary entries to a file
+	 * 
+	 */
+	public void saveDiaryEntries()
+	{
+		
+	}
+	
+	/*
+	 * 
+	 * 
+	 */
+	public void openFromDisk()
+	{
+		
+	}
+	
+	/*
+	 * method to search for an Employee's meetings
+	 * 
+	 */
+	public void searchEmployeeMeetings()
+	{
+		
+	}
+	
+	/*
+	 * method to print Employee's diary entries
+	 * 
+	 */
+	public void printEmployeeEntries()
+	{
+		
+	}
+}
